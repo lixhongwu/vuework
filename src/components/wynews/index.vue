ebcf817163cd4adf229cf419747b188e2b615180
lixhongwu <lixhongwu@163.com>
2018-06-10 11:42:00 +0800

    1

---
+++ src/components/wynews/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="head">
+    <ul class="head-content">
+      <li class="head-item">推荐</li>
+      <li class="head-item">新闻</li>
+      <li class="head-item">娱乐</li>
+      <li class="head-item">体育</li>
+      <li class="head-item">图片</li>
+      <li class="head-item">财经</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style style="less">
+  @import "index.scss";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import wynews from '@/components/wynews/index'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/news',
+      name: 'news',
+      component: wynews
     }
   ]
 })
