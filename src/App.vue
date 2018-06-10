6abd26d72146b189104c61a03d3f8691901be06f
lixhongwu <lixhongwu@163.com>
2018-06-10 16:07:25 +0800

    增加网易demo屏幕自适应

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,16 @@
 </template>
 
 <script>
-export default {
+  let htmlWidth = document.documentElement.clentWidth || document.body.clientWidth;
+  let htmlDom = document.getElementsByTagName('html')[0];
+  htmlDom.style.fontSize = htmlWidth / 10 +'px';
+  window.addEventListener('resize',(e) => {
+    let htmlWidth = document.documentElement.clentWidth || document.body.clientWidth;
+    htmlDom.style.fontSize = htmlWidth / 10 +'px';
+  });
+  export default {
   name: 'App'
+
 }
 </script>
 
--- src/components/wynews/index.vue
+++ src/components/wynews/index.vue
@@ -1,13 +1,56 @@
 <template>
-  <div class="head">
-    <ul class="head-content">
-      <li class="head-item">推荐</li>
-      <li class="head-item">新闻</li>
-      <li class="head-item">娱乐</li>
-      <li class="head-item">体育</li>
-      <li class="head-item">图片</li>
-      <li class="head-item">财经</li>
-    </ul>
+  <div>
+    <div class="head">
+      <ul class="head-content">
+        <li class="head-item">推荐</li>
+        <li class="head-item">新闻</li>
+        <li class="head-item">娱乐</li>
+        <li class="head-item">体育</li>
+        <li class="head-item">图片</li>
+        <li class="head-item">财经</li>
+      </ul>
+    </div>
+    <div class="banner-cotent">
+      <img class="banner" src="../../assets/banner.jpeg"/>
+      <p class="title">韩国民众举行集体祈福</p>
+    </div>
+    <div class="new-content">
+      <ul>
+        <li class="new-items">
+          <div class="item-inner">
+            <img class="img-inner" src="../../assets/1.png">
+            <p class="item-title">国礼送什么？普京带了一个俄罗斯浴室</p>
+            <span class="time">1小时前</span>
+              <span class="num">266跟帖</span>
+          </div>
+        </li>
+        <li class="new-items">
+          <div class="item-inner">
+            <img class="img-inner" src="../../assets/1.png">
+            <p class="item-title">国礼送什么？普京带了一个俄罗斯浴室</p>
+            <span class="time">1小时前</span>
+            <span class="num">266跟帖</span>
+          </div>
+        </li>
+        <li class="new-items">
+          <div class="item-inner">
+            <img class="img-inner" src="../../assets/1.png">
+            <p class="item-title">国礼送什么？普京带了一个俄罗斯浴室</p>
+            <span class="time">1小时前</span>
+            <span class="num">266跟帖</span>
+          </div>
+        </li>
+        <li class="new-items">
+          <div class="item-inner">
+            <img class="img-inner" src="../../assets/1.png">
+            <p class="item-title">国礼送什么？普京带了一个俄罗斯浴室</p>
+            <span class="time">1小时前</span>
+            <span class="num">266跟帖</span>
+          </div>
+        </li>
+      </ul>
+
+    </div>
   </div>
 </template>
 <script>
@@ -22,6 +65,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style style="less">
+<style  lang="scss" >
   @import "index.scss";
 </style>
