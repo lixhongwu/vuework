e7d144e9db50a6e121438363c1e24647acb479cc
lixhongwu <lixhongwu@163.com>
2018-06-29 22:24:04 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,16 @@
 </template>
 
 <script>
-  let htmlWidth = document.documentElement.clentWidth || document.body.clientWidth;
-  let htmlDom = document.getElementsByTagName('html')[0];
-  htmlDom.style.fontSize = htmlWidth / 10 +'px';
-  window.addEventListener('resize',(e) => {
-    let htmlWidth = document.documentElement.clentWidth || document.body.clientWidth;
-    htmlDom.style.fontSize = htmlWidth / 10 +'px';
-  });
-  export default {
+// 屏幕自适应
+let htmlWidth = document.documentElement.clentWidth || document.body.clientWidth
+let htmlDom = document.getElementsByTagName('html')[0]
+htmlDom.style.fontSize = htmlWidth / 10 + 'px'
+window.addEventListener('resize', (e) => {
+  let htmlWidth = document.documentElement.clentWidth || document.body.clientWidth
+  htmlDom.style.fontSize = htmlWidth / 10 + 'px'
+})
+export default {
   name: 'App'
-
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import wynews from '@/components/wynews/index'
+import news from '@/components/wynews/news'
+import test from '@/components/test/HelloWorld'
 
 Vue.use(Router)
 
@@ -15,7 +16,12 @@ export default new Router({
     {
       path: '/news',
       name: 'news',
-      component: wynews
+      component: news
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: test
     }
   ]
 })
