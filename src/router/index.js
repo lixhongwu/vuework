c2e776dd5b8b597be88dc851f7ec3f039d1b81d7
lixhongwu <lixhongwu@163.com>
2018-06-29 22:28:33 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import news from '@/components/wynews/news'
-import test from '@/components/test/HelloWorld'
 
 Vue.use(Router)
 
@@ -17,11 +16,6 @@ export default new Router({
       path: '/news',
       name: 'news',
       component: news
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: test
     }
   ]
 })
